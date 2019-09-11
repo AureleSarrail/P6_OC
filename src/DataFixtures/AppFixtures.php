0e068cfdbb0b7c59a5c115b2d4c9988cb7f80774
Aurele Sarrail <aurele.sarrail@gmail.com>
2019-09-11 11:17:26 +0200

    fixtures made for Trick,Image,Vidéo and category. User and comment are not necessary now.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Image;
+use App\Entity\Trick;
+use App\Entity\Video;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +13,44 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //On crée 3 catégories
+        for ($i=0; $i < 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            //on créé 5 tricks par catégories
+            for ($j=0; $j<5; $j++) {
+                $trick = new Trick();
+                $trick->setTrickname($faker->sentence())
+                    ->setDescription($faker->paragraph())
+                    ->setCreatedAt($faker->dateTimeBetween('-3 months'))
+                    ->setUpdatedAt(new \DateTime())
+                    ->setCategory($category);
+
+                $manager->persist($trick);
+
+                //on créée 5 images et 2 vidéos par tricks
+                for ($k=0; $k < 5; $k++) {
+                    $image = new Image();
+                    $image->setUrl($faker->imageUrl())
+                        ->setTrick($trick);
+
+                    $manager->persist($image);
+                }
+                for ($l=0; $l<2; $l++) {
+                    $video = new Video();
+                    $video->setUrl('https://www.youtube.com/watch?v=1TJ08caetkw')
+                        ->setTrick($trick);
+                    $manager->persist($video);
+                }
+            }
+        }
 
         $manager->flush();
     }
