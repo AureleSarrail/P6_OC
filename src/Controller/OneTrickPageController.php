6a242fcae9da47f2f2da1e304a2827c3304750e9
Aurele Sarrail <aurele.sarrail@gmail.com>
2019-09-17 16:03:05 +0200

    The route is working and oneTrickById function is working in the repo. Now i will organize the one trick page.

--- src/Controller/OneTrickPageController.php
+++ src/Controller/OneTrickPageController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OneTrickPageController extends AbstractController
 {
     /**
-     * @Route("/show/{$id)", name="show")
+     * @Route("/show/{id}", name="show")
      */
-    public function index()
+    public function index($id, TrickRepository $repo)
     {
         return $this->render('one_trick_page/index.html.twig', [
-            'controller_name' => 'OneTrickPageController',
+            'Trick' => $repo->oneTrickById($id)
         ]);
     }
 }
