c08cbeebf9470bf63014cf6ccd2a4034c997567f
Aurele Sarrail <aurele.sarrail@gmail.com>
2019-10-15 16:57:53 +0200

    Users can now create an account. Now i'll try to add a forgotten password functionality.

---
+++ src/Validator/PasswordValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class PasswordValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+
+//        if ($value == $verif) {
+//            return true;
+//        }
+
+        // TODO: implement the validation here
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $value)
+            ->addViolation();
+    }
+}
